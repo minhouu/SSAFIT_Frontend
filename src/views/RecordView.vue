5e1d1a2d81d4c18882a16211599f5d801f3eb447
minhouu <tortor1403@gmail.com>
2023-11-24 09:02:20 +0900

    minor changes

--- src/views/RecordView.vue
+++ src/views/RecordView.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
+  <div v-if="userStore.user">
     <div class="row">
       <div class="col-2">
         <RecodrNav />
       </div>
       <div class="col-10">
         <RouterView />
-      </div>
+      </div>    </div>
+  </div>
 
-
-    </div>
-
+  <div class="container" v-else>
+    <h2 class="fs-3 fw-bold">로그인 후 이용해주세요</h2>
   </div>
 </template>
 
@@ -18,7 +18,7 @@
 import RecodrNav from '@/components/record/nav/RecodeNav.vue'
 import { useUserStore } from '../stores/user';
 
-const UserStore = useUserStore();
+const userStore = useUserStore();
 
 </script>
 
